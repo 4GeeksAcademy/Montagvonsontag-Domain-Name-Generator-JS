7fac20a10e163f4bcef9a9840b8da38cc41cfe4d
Montagvonsontag <37384660+Montagvonsontag@users.noreply.github.com>
2025-03-15 22:34:37 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon', 'telecom'];
+const domain = ['es', 'com', 'io', 'net', 'us'];
+
+for (let i in pronoun){
+  for (let j in adj){
+    for (let k in noun){
+      for (let l in domain){
+        let fulldomain=(pronoun[i]+adj[j]+noun[k]+"."+domain[l]);
+        console.log(fulldomain);  
+        
+      }
+    }
+  } 
+}
+
+}
